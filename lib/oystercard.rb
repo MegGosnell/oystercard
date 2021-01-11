e38601b587f18b85ef079646ee0134375422660d
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-11 16:47:51 +0000

    charging for the journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
 class Oystercard
     MAXIMUM_BALANCE = 90
+    MINIMUM_BALANCE = 1
   def initialize(max = MAXIMUM_BALANCE)
     @max = max
     @balance = 0
@@ -11,16 +12,14 @@ class Oystercard
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
   def touch_in
     raise "Already in use" if in_journey?
+    fail "insufficient funds" if @balance < MINIMUM_BALANCE
     @in_use = true
   end
 
   def touch_out
+    deduct(4)
     @in_use = false
   end
 
@@ -29,4 +28,9 @@ class Oystercard
   end
 
   attr_reader :balance, :max, :in_use
-end
+
+private
+def deduct(amount)
+    @balance -= amount
+  end
+end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,14 +15,8 @@ describe Oystercard do
     expect { subject.top_up (1) }.to raise_error "maximum balance reached #{subject.max}"
   end
 
-  it 'decreases balance by topped up amount' do
-    prior_balance = subject.balance
-    amount = 3
-    subject.deduct(amount)
-    expect(subject.balance).to eq (prior_balance - amount)
-  end
-
   it 'touches in' do
+    subject.top_up(3)
     subject.touch_in
     expect(subject.in_use).to eq true
   end
@@ -33,7 +27,15 @@ describe Oystercard do
   end
 
   it 'knows when is in use' do
+    subject.top_up(3)
     subject.touch_in
     expect { subject.touch_in}.to raise_error "Already in use"
+  end
+  it "will not work with insufficent funds" do 
+    expect { subject.touch_in}.to raise_error "insufficient funds"
+  end 
+
+  it 'will deduct the correct amount' do 
+    expect {subject.touch_out }.to change{subject.balance}.by(-4)
   end
 end
